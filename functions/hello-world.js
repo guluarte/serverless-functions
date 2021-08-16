b847ac7575e10297421297c706aff13eedc1202a
Rod Hale <rod@rllinsure.com>
2021-08-16 03:29:51 -0600

    test

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,7 +1,7 @@
 exports.handler = async () => {
+  console.log(process.env.TEST_VALUE)
   return {
     statusCode: 200,
     body: 'Hello egghead'
   }
 }
-
---
+++ functions/load-corgis.js
@@ -0,0 +1,37 @@
+const fetch = require('node-fetch')
+
+exports.handler = async () => {
+  const corgis = await fetch('https://no-cors-api.netlify.app/api/corgis').then(
+    res => res.json()
+  )
+
+  const unsplashPromise = await fetch(
+    'https://api.unsplash.com/collections/48405776/photos',
+    {
+      headers: {
+        Authorization: `Client-ID ${process.env.UNSPLASH_ACESS_KEY}`
+      }
+    }
+  ).then(res => res.json())
+
+  const [unsplashData] = await Promise.all([unsplashPromise])
+
+  const completeData = corgis.map(corgi => {
+    const photo = unsplashData.find(p => corgi.id === p.id)
+
+    return {
+      ...corgi,
+      alt: photo.alt_description,
+      credit: photo.user.name,
+      url: `${photo.urls.raw}&auto=format&fit=crop&w=300&h=300&q=80&crop=entropy`
+    }
+  })
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(completeData)
+  }
+}
